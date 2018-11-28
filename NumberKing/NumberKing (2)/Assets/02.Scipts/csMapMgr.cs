dadbe050e39d6501844a2ad9ea7f7bb9487a9794
apolloscat07 <45222049+apolloscat07@users.noreply.github.com>
2018-11-28 17:58:57 +0900

    numberking

---
+++ NumberKing/NumberKing (2)/Assets/02.Scipts/csMapMgr.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class csMapMgr
+{
+
+	private static csMapMgr m_Instance;
+	private csMapData[] MapDatats;
+	private csMapData tempData;
+	private int MapDataCount = 0;
+
+	public static csMapMgr GetInstance()
+	{
+		if (m_Instance == null) {
+			m_Instance = new csMapMgr ();
+		}
+		return m_Instance;
+	}
+
+	public void MapInit()
+	{
+		MapDatats = Resources.LoadAll<csMapData> ("MapData");
+
+	}
+
+	public csMapData MapSetting(int _StageLevel)
+	{
+		MapInit ();
+		for (int i = 0; i < MapDatats.Length; ++i) {
+			if (MapDatats [i].name.Contains (_StageLevel.ToString())) {
+				
+				tempData = MapDatats [i];
+				break;
+			} else {
+				return null;
+			}
+		}
+		return tempData;
+	}
+}
