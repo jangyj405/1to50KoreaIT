2723e1807db5c0a3774311f0f7743074d629594a
jangyj405 <jangyj405@naver.com>
2018-11-28 16:06:39 +0900

    Numberking folder delete

---
+++ ServerTest/Assets/JooAssets/02.Scripts/CJooBackend/CJooGameInfo.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BackEnd;
+
+
+public class CJooGameInfo : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start()
+	{
+		/*
+		//Test
+		Param param = new Param();
+		Dictionary<string, string> message = new Dictionary<string, string>()
+		{
+			{ "asdf", "asdf" },
+			{ "qwer", "qwer"}
+		};
+
+		param.Add("20181127", message);
+		param.Add("1234", false);
+		Backend.GameInfo.Insert("message", param);
+		JsonUtility.FromJson<Dictionary<string, string>>(param.GetJson());
+		foreach(var item in message.Keys)
+		{
+			Debug.Log(item);
+		}
+		*/
+		
+	}
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+}
---
+++ ServerTest/Assets/JooAssets/02.Scripts/CJooBackend/CJooServerDataTables.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class CJooServerDataTables
+{
+	public const string stageModeScore = "StageClearDic";
+	public const string infiniteModeScore = "InfiniteMode";
+	public const string selectedCharacter = "Character";
+	
+}
