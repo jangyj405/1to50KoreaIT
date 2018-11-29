3087606556c8f3d44d1e0f16b1e840061377576b
jangyj405 <jangyj405@naver.com>
2018-11-29 17:05:05 +0900

    Mailing System Making

---
+++ ServerTest/Assets/JooAssets/02.Scripts/CJooBackend/CJooMail.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+
+[Serializable]
+public class JsonNum
+{
+	[SerializeField]
+	public int N;
+}
+
+
+
+[Serializable]
+public class CJooPostFromAdmin
+{
+	[SerializeField]
+	public JsonS content;
+
+	[SerializeField]
+	public JsonS expirationDate;
+
+	[SerializeField]
+	public JsonS inDate;
+
+	[SerializeField]
+	public JsonTest test;
+
+	[SerializeField]
+	public JsonS sentDate;
+
+	[SerializeField]
+	public JsonS receiverNickname;
+
+	[SerializeField]
+	public JsonS title;
+	
+}
+
+
+
+
+
+
+public class CJooMail : MonoBehaviour
+{
+	void OnEnable()
+	{
+
+	}
+	// Use this for initialization
+	void Start ()
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+
+	void GetPostListFromServer()
+	{
+
+	}
+
+}
