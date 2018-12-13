28e21d3ec81a78d4df34a14651df9b8a98e0a8d0
jangyj405 <jangyj405@naver.com>
2018-12-13 19:32:21 +0900

    장Black우석의 유산 수정
    Stage씬 수정해야함

---
+++ ServerTest/Assets/JooAssets/02.Scripts/CJooOneStageButton.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CJooOneStageButton : MonoBehaviour
+{
+	private int stage;
+
+	[SerializeField]
+	private Text stageText = null;
+
+	[SerializeField]
+	private Text recordText = null;
+	public string RecordText
+	{
+		get
+		{
+			return recordText.text;
+		}
+		set
+		{
+			recordText.text = value;
+		}
+	}
+
+
+	[SerializeField]
+	private Button stageButton = null;
+
+	public int Stage
+	{
+		get
+		{
+			return stage;
+		}
+		set
+		{
+			stage = value;
+			stageText.text = string.Format("Stage{0}", stage.ToString());
+			//todo record Text Setting
+
+		}
+	}
+
+	public void OnClickBtnSelectStage()
+	{
+		StageView.stageView.StageClick();
+		StageView.stageView.SelectedStage = Stage;
+	}
+}
--- ServerTest/Assets/JooAssets/02.Scripts/SceneMgr/StageView.cs
+++ ServerTest/Assets/JooAssets/02.Scripts/SceneMgr/StageView.cs
@@ -3,18 +3,95 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.EventSystems;
+using System;
+using BackEnd;
+using Newtonsoft.Json;
+using System.Linq;
+using System.Linq.Expressions;
 
-public class StageView : MonoBehaviour {
+[Serializable]
+public class JsonTableStage : JsonTableBase
+{
+	[SerializeField]
+	public Dictionary<string, int> StageRecord;
+}
+
+[Serializable]
+public class JsonTableStageRow
+{
+	[SerializeField]
+	public JsonTableStage[] row;
+}
+
+
+
+
+
+
+
+public class StageView : MonoBehaviour
+{
+	public static StageView stageView = null;
+	public int SelectedStage;
 
     public GameObject startScreen;
-
+	public CJooOneStageButton[] stageButtons = null;
+	private int radix = 2;
+	private Dictionary<string, int> stageDict;
+	public KeyValuePair<string, int>[] stageKVPair;
 	void Start () 
     {
         SceneManager.LoadScene("AddtiveSceneETC", LoadSceneMode.Additive);
+
+		for (int i = 0; i < stageButtons.Length; i++)
+		{
+			stageButtons[i].Stage = radix * 10 + i;
+			stageButtons[i].RecordText = "-";
+		}
+		//todo 
+		stageDict = GetStageDataFromServer();
+		//받은 값으로 세팅하기
+		//값이 없으면(기록이 없으면) 버튼 interactable false
+		string tFormat = string.Format("Stage_{00}", radix);
+		if(stageDict != null)
+		{
+			 stageKVPair = stageDict.Where((t) => t.Key.Contains(tFormat))
+															.OrderBy((t) => t.Key)
+															.Select((t) => t)
+															.ToArray();
+													
+			for(int i =0; i < stageKVPair.Length;i++)
+			{
+				stageButtons[i].RecordText = (stageKVPair[i].Value * 0.01).ToString("000.00");
+			}
+		}
+	}
+
+	private Dictionary<string, int> GetStageDataFromServer()
+	{
+		BackendReturnObject bro = Backend.GameInfo.GetPublicContents("stage");
+		string tReturnValue = bro.GetReturnValue();
+		JsonTableStageRow stageData = JsonConvert.DeserializeObject<JsonTableStageRow>(tReturnValue);
+		try
+		{
+			return stageData.row[0].StageRecord;
+		}
+		catch
+		{
+			return null;
+		}
+	}
+
+
+	void Awake()
+	{
+		stageView = this;
 	}
 
     public void StageClick()
     {
+		startScreen.SetActive(true);
+		/*
         startScreen.SetActive(true);
 		//Debug.Log (EventSystem.current.currentSelectedGameObject.name);
 		if ((int.Parse(EventSystem.current.currentSelectedGameObject.name.Substring (12)) > CRyuGameDataMgr.GetInst ().GetMapStageLevel)) 
@@ -26,7 +103,10 @@ public class StageView : MonoBehaviour {
 			csMapMgr.GetInstance ().MapSetting (CRyuGameDataMgr.GetInst ().GetMapStageLevel);
 		}
 		Debug.Log (CRyuGameDataMgr.GetInst().GetMapStageLevel);
-    }
+		*/
+
+
+	}
 
     public void StageCloseClick()
     {
@@ -35,7 +115,8 @@ public class StageView : MonoBehaviour {
 
     public void GameStartClick()
     {
-        SceneManager.LoadScene("GameScene");
+		csMapMgr.GetInstance().MapSetting(SelectedStage);
+		SceneManager.LoadScene("GameScene");
     }
 		
 }
