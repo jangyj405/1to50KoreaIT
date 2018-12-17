069b1b45897c80188a810cdd46c909427006ee59
apolloscat07 <45222049+apolloscat07@users.noreply.github.com>
2018-12-17 14:15:56 +0900

    sound

---
+++ ServerTest/Assets/JooAssets/02.Scripts/SceneMgr/SoundManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour {
+
+    public static SoundManager instance = null;
+    public AudioSource BGMAudioSource;
+    public AudioSource SFXAudioSource;
+
+    public AudioClip BGMMainMenuAudioClip;
+    public AudioClip[] BGMGameSceneAudioClip;
+
+    void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else
+            Destroy(gameObject);
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    void Start()
+    {
+        MainMenuBGM();
+    }
+
+    public void MainMenuBGM()
+    {
+        BGMAudioSource.clip = BGMMainMenuAudioClip;
+        BGMAudioSource.Play();
+    }
+
+    public void GameSceneBGM()
+    {
+        int index = Random.Range(0, BGMGameSceneAudioClip.Length);
+        BGMAudioSource.clip = BGMGameSceneAudioClip[index];
+        BGMAudioSource.Play();
+    }
+}
