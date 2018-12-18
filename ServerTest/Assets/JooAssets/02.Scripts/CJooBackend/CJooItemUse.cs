94f0854576516bd9df4c567ae38c8e9f62d4efdc
jangyj405 <jangyj405@naver.com>
2018-12-18 15:45:28 +0900

    서버에서 아이템 리스트 받아서 세팅

---
+++ ServerTest/Assets/JooAssets/02.Scripts/CJooBackend/CJooItemUse.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using BackEnd;
+using Newtonsoft.Json;
+
+public class CJooItemUse : MonoBehaviour
+{
+	public CJooOneItemButton[] items = null;
+	// Use this for initialization
+	void Start ()
+	{
+		SetItemsFromServer();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+
+	void SetItemsFromServer()
+	{
+		BackendReturnObject bro = Backend.GameInfo.GetPrivateContents("item");
+		string tReturnValue = bro.GetReturnValue();
+		ItemFromServerRows tItems = JsonConvert.DeserializeObject<ItemFromServerRows>(tReturnValue);
+		
+		try
+		{
+			for (int i = 0; i < items.Length; i++)
+			{
+				items[i].ItemCount = tItems.rows[0].itemDict.M[string.Format("item{0}", (i + 1).ToString("00"))].ToInt();
+			}
+		}
+		catch
+		{
+			for (int i = 0; i < items.Length; i++)
+			{
+				items[i].ItemCount = 0;
+			}
+		}
+	}
+}
---
+++ ServerTest/Assets/JooAssets/02.Scripts/CJooBackend/CJooOneItemButton.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CJooOneItemButton : MonoBehaviour
+{
+	public Text txtCount = null;
+	public Toggle toggle = null;
+	private int itemCount = 0;
+	public int ItemCount
+	{
+		get
+		{
+			return itemCount;
+		}
+		set
+		{
+			itemCount = value;
+			txtCount.text = itemCount.ToString();
+			if(itemCount <= 0)
+			{
+				toggle.interactable = false;
+			}
+		}
+	}
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnDisable()
+	{
+		toggle.isOn = false;
+	}
+}
