714ced597344fb71b705d2af5237cccce9ad9cac
jangyj405 <jangyj405@naver.com>
2018-12-26 14:47:41 +0900

    인앱 결제 구현
    테스트 해야함

--- ServerTest/Assets/JooAssets/02.Scripts/CJooBackend/CJooIAP.cs
+++ ServerTest/Assets/JooAssets/02.Scripts/CJooBackend/CJooIAP.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Purchasing;
+using System;
+using BackEnd;
 
 public class CJooIAP : MonoBehaviour, IStoreListener
 {
@@ -9,15 +11,19 @@ public class CJooIAP : MonoBehaviour, IStoreListener
 	private IExtensionProvider provider = null;
 	public static readonly string[] productIDs = { "Dia010", "Dia033", "Dia055", "Dia120", "Dia180", "Dia300" };
 
+	[SerializeField]
+	private GameObject disableTouchPanel = null;
 
 	public void OnInitialized(IStoreController controller, IExtensionProvider extensions)
 	{
-		throw new System.NotImplementedException();
+		this.controller = controller;
+		this.provider = extensions;	
 	}
 
 	public void OnInitializeFailed(InitializationFailureReason error)
 	{
-		throw new System.NotImplementedException();
+		//todo Display UI
+		Debug.Log("OnInitializeFailed InitializationFailureReason:" + error);
 	}
 
 	public void OnPurchaseFailed(Product i, PurchaseFailureReason p)
@@ -25,9 +31,35 @@ public class CJooIAP : MonoBehaviour, IStoreListener
 		throw new System.NotImplementedException();
 	}
 
-	public PurchaseProcessingResult ProcessPurchase(PurchaseEventArgs e)
+	public PurchaseProcessingResult ProcessPurchase(PurchaseEventArgs args)
 	{
-		throw new System.NotImplementedException();
+		// A consumable product has been purchased by this user.
+		bool isContains = false;
+		for (int i = 0; i < productIDs.Length; i++)
+		{
+			if (productIDs[i].Equals(args.purchasedProduct.definition.id, StringComparison.Ordinal))
+			{
+				isContains = true;
+				break;
+			}
+		}
+
+		if (isContains)
+		{
+			Debug.Log(string.Format("ProcessPurchase: PASS. Product: '{0}'", args.purchasedProduct.definition.id));
+			// The consumable item has been successfully purchased, add 100 coins to the player's in-game score.
+			Backend.TBC.ChargeTBC(args.purchasedProduct.receipt, args.purchasedProduct.definition.id);
+		}
+		else
+		{
+			Debug.Log(string.Format("ProcessPurchase: FAIL. Unrecognized product: '{0}'", args.purchasedProduct.definition.id));
+		}
+
+		// Return a flag indicating whether this product has completely been received, or if the application needs 
+		// to be reminded of this purchase at next app launch. Use PurchaseProcessingResult.Pending when still 
+		// saving purchased products to the cloud, and when that save is delayed. 
+		disableTouchPanel.SetActive(false);
+		return PurchaseProcessingResult.Complete;
 	}
 
 	// Use this for initialization
@@ -35,12 +67,12 @@ public class CJooIAP : MonoBehaviour, IStoreListener
 	{
 		if(controller == null)
 		{
-		
+			InitializePurchasing();
 		}
 	}
 	public void InitializePurchasing()
 	{
-		/*
+		
 		// If we have already connected to Purchasing ...
 		if (IsInitialized())
 		{
@@ -51,24 +83,11 @@ public class CJooIAP : MonoBehaviour, IStoreListener
 		// Create a builder, first passing in a suite of Unity provided stores.
 		var builder = ConfigurationBuilder.Instance(StandardPurchasingModule.Instance());
 
-		// Add a product to sell / restore by way of its identifier, associating the general identifier
-		// with its store-specific identifiers.
-		builder.AddProduct(kProductIDConsumable, ProductType.Consumable);
-		// Continue adding the non-consumable product.
-		builder.AddProduct(kProductIDNonConsumable, ProductType.NonConsumable);
-		// And finish adding the subscription product. Notice this uses store-specific IDs, illustrating
-		// if the Product ID was configured differently between Apple and Google stores. Also note that
-		// one uses the general kProductIDSubscription handle inside the game - the store-specific IDs 
-		// must only be referenced here. 
-		builder.AddProduct(kProductIDSubscription, ProductType.Subscription, new IDs(){
-				{ kProductNameAppleSubscription, AppleAppStore.Name },
-				{ kProductNameGooglePlaySubscription, GooglePlay.Name },
-			});
-
-		// Kick off the remainder of the set-up with an asynchrounous call, passing the configuration 
-		// and this class' instance. Expect a response either in OnInitialized or OnInitializeFailed.
+		for (int i = 0; i < productIDs.Length; i++)
+		{
+			builder.AddProduct(productIDs[i], ProductType.Consumable);
+		}
 		UnityPurchasing.Initialize(this, builder);
-		*/
 	}
 
 	private bool IsInitialized()
@@ -76,6 +95,43 @@ public class CJooIAP : MonoBehaviour, IStoreListener
 		// Only say we are initialized if both the Purchasing references are set.
 		return controller != null && provider != null;
 	}
+
+	public void BuyProductID(string productId)
+	{
+		disableTouchPanel.SetActive(true);
+		// If Purchasing has been initialized ...
+		if (IsInitialized())
+		{
+			// ... look up the Product reference with the general product identifier and the Purchasing 
+			// system's products collection.
+			Product product = controller.products.WithID(productId);
+
+			// If the look up found a product for this device's store and that product is ready to be sold ... 
+			if (product != null && product.availableToPurchase)
+			{
+				Debug.Log(string.Format("Purchasing product asychronously: '{0}'", product.definition.id));
+				// ... buy the product. Expect a response either through ProcessPurchase or OnPurchaseFailed 
+				// asynchronously.
+				controller.InitiatePurchase(product);
+			}
+			// Otherwise ...
+			else
+			{
+				// ... report the product look-up failure situation  
+				Debug.Log("BuyProductID: FAIL. Not purchasing product, either is not found or is not available for purchase");
+				disableTouchPanel.SetActive(false);
+			}
+		}
+		// Otherwise ...
+		else
+		{
+			// ... report the fact Purchasing has not succeeded initializing yet. Consider waiting longer or 
+			// retrying initiailization.
+			Debug.Log("BuyProductID FAIL. Not initialized.");
+			disableTouchPanel.SetActive(false);
+		}
+	}
+
 
 	// Update is called once per frame
 	void Update ()
