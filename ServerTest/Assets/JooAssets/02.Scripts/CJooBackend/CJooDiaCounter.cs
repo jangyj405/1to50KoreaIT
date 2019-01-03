11f4d37b062097705f540f128dbd614dd8e3d1fb
jangyj405 <jangyj405@naver.com>
2019-01-03 17:12:04 +0900

    일단 커밋

---
+++ ServerTest/Assets/JooAssets/02.Scripts/CJooBackend/CJooDiaCounter.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BackEnd;
+
+public static class CJooDiaCounter
+{
+	public static int GetTBCAmount()
+	{
+		string tbcAmount = Backend.TBC.GetAmountTBC();
+		Debug.Log(tbcAmount);
+		return 0;
+	}
+	
+}
