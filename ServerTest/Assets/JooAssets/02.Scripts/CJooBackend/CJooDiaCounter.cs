d970fdf97aaf4eda539c0d27b3c25ac1ea55d020
jangyj405 <jangyj405@naver.com>
2019-01-07 17:51:59 +0900

    프리팹 및 dia display 수정

--- ServerTest/Assets/JooAssets/02.Scripts/CJooBackend/CJooDiaCounter.cs
+++ ServerTest/Assets/JooAssets/02.Scripts/CJooBackend/CJooDiaCounter.cs
@@ -9,20 +9,39 @@ using System;
 
 public static class CJooDiaCounter
 {
-	public static int GetTBCAmount()
+	public static string GetTBCAmount()
 	{
+		/*
 		string tbcAmount = Backend.TBC.GetAmountTBC();
 		string strTmp = Regex.Replace(tbcAmount, @"\D", "");
 		int nTmp = 0;
 		try
 		{
-			 nTmp = int.Parse(strTmp);
+			nTmp = int.Parse(strTmp);
 		}
 		catch
 		{
 			nTmp = 0;
 		}
 		return nTmp;
+		*/
+
+		BackendReturnObject bro = Backend.TBC.GetTBC();
+		LitJson.JsonData tbcData = bro.GetReturnValuetoJSON();
+		string tbc = tbcData["amountTBC"].ToString();
+		return tbc;
+
+		/*
+		int ttbc = 0;
+		try
+		{
+			ttbc = int.Parse(tbc);
+		}
+		catch
+		{
+			ttbc = 0;
+		}
+		return ttbc;
+		*/
 	}
-	
 }
